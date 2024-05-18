6d5c9f8ee004462766520028d066c7efc884b2e4
Michele Nuzzi <michele.nuzzi.2014@gmail.com>
2024-05-18 10:31:06 +0200

    fix eqUPLCTerm already exported in uplc dep

--- src/CEKValue/CEKValue.ts
+++ src/CEKValue/CEKValue.ts
@@ -1,4 +1,4 @@
-import { UPLCConst, constTypeEq, canConstValueBeOfConstType, eqConstValue, ErrorUPLC, Case, Constr, UPLCTerm, Application, Builtin, Delay, Force, Lambda, UPLCVar } from "@harmoniclabs/uplc";
+import { constTypeEq, canConstValueBeOfConstType, eqConstValue, ErrorUPLC, Case, Constr, UPLCTerm, Application, Builtin, Delay, Force, Lambda, UPLCVar, eqUPLCTerm } from "@harmoniclabs/uplc";
 import { PartialBuiltin } from "../BnCEK/PartialBuiltin";
 import { CEKDelay } from "./CEKDelay";
 import { CEKLambda } from "./CEKLambda";
@@ -73,53 +73,6 @@ export function eqCEKValue( a: Readonly<CEKValue>, b: Readonly<CEKValue> ): bool
         a.tag === b.tag &&
         a.nMissingArgs === b.nMissingArgs &&
         a.args.every( (arg, i) => eqCEKValue( arg, b.args[ i ] ) )
-    );
-    
-    return false;
-}
-
-// type UPLCTerm = UPLCVar | Delay | Lambda | Application | UPLCConst | Force | ErrorUPLC | Builtin | Constr | Case;
-export function eqUPLCTerm( a: UPLCTerm, b: UPLCTerm ): boolean
-{
-    if( a instanceof UPLCVar && b instanceof UPLCVar) return a.deBruijn === b.deBruijn;
-    if( a instanceof Delay && b instanceof Delay ) return eqUPLCTerm( a.delayedTerm, b.delayedTerm )
-    if( a instanceof Lambda && b instanceof Lambda) return eqUPLCTerm( a.body, b.body );
-    if( a instanceof Application && b instanceof Application )
-    return (
-        eqUPLCTerm( a.argTerm, b.argTerm ) &&
-        eqUPLCTerm( a.funcTerm, b.funcTerm )
-    );
-    if( a instanceof UPLCConst && b instanceof UPLCConst )
-    return (
-        constTypeEq( a.type, b.type ) &&
-        canConstValueBeOfConstType( a.value, a.type ) &&
-        canConstValueBeOfConstType( b.value, b.type ) &&
-        (() => {
-            try {
-                return eqConstValue( a.value, b.value );
-            } catch (e) {
-                if( e instanceof RangeError ) return false;
-
-                throw e;
-            }
-        })()
-    );
-    if( a instanceof Force && b instanceof Force ) return eqUPLCTerm( a.termToForce, b.termToForce );
-    if( a instanceof ErrorUPLC ) return b instanceof ErrorUPLC;
-    if( a instanceof Builtin && b instanceof Builtin ) return a.tag === b.tag;
-    
-    if( a instanceof Constr && b instanceof Constr )
-    return (
-        a.index === b.index &&
-        a.terms.length === b.terms.length &&
-        a.terms.every((t,i) => eqUPLCTerm( t, b.terms[i] ))
-    );
-
-    if( a instanceof Case && b instanceof Case )
-    return (
-        eqUPLCTerm( a.constrTerm, b.constrTerm ) &&
-        a.continuations.length === b.continuations.length &&
-        a.continuations.every((t,i) => eqUPLCTerm( t, b.continuations[i] ))
     );
     
     return false;
